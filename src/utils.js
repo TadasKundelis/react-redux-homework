57bd6996215d7cb357e93852aed4528994faa361
Tadas Kundelis <tkundelis@csc.com>
2018-12-29 04:03:33 +0200

    added default export

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,17 @@
 export const getFormatedDate = () => {
-    const date = new Date();
-    const [year, month, day, hour, minute, second] = [
-        date.getFullYear(), 
-        date.getMonth() + 1, 
-        date.getDate(), 
-        date.getHours(), 
-        date.getMinutes(), 
-        date.getSeconds()
-    ].map(padDate);
-    
-    return `${year}-${month}-${day} ${hour}:${minute}:${second}: `;
-}
+  const date = new Date();
+  const [year, month, day, hour, minute, second] = [
+    date.getFullYear(),
+    date.getMonth() + 1,
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
+  ].map(padDate);
 
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}: `;
+};
 
-const padDate = date => date < 10 ? `0${date}` : date;
+const padDate = date => (date < 10 ? `0${date}` : date);
+
+export default getFormatedDate;
